8a15c22b5baabd530e043dcb8885e73a5b56d2e1
syedabdulbasi7 <54896018+SyedAbdulBasit7@users.noreply.github.com>
2024-08-19 10:28:55 +0500

    Update on_boarding_screen.dart

--- lib/on_boarding_screen.dart
+++ lib/on_boarding_screen.dart
@@ -12,58 +12,80 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  O3DController o3dController = O3DController();
-  int page = 0;
-  String animationName = 'WalkClean';
-  double _value = -2; // Initial value
-  late Timer _timer;
+  final O3DController o3dController = O3DController();
+  final double initialValue = -2.0;
+  final double maxValue = 12.0;
+  final double resetValue = -11.0;
+  final double smoothIncrement = 0.1;
+  final int timerDurationMs = 50;
+  final int resetDelayMs = 500;
+  final int postResetDelayMs = 700;
   bool _isResetting = false;
+  late double _value;
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
-    _startSmoothTimer(); // Start the timer when the widget is initialized
+    _value = initialValue;
+    _startSmoothTimer();
   }
 
   void _startSmoothTimer() {
-    _timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+    _timer = Timer.periodic(Duration(milliseconds: timerDurationMs), (timer) {
       if (_isResetting) return;
+
       setState(() {
-        _value += 0.1; // Smaller increment for smoother transition
-        o3dController.cameraTarget(0.20, 1.5, _value);
-        if (_value > 12) {
+        _value += smoothIncrement;
+        _updateCameraTarget(_value);
+        if (_value > maxValue) {
           _isResetting = true;
-          _smoothReset(); // Start the smooth reset process
+          _smoothReset();
         }
       });
     });
   }
 
   void _smoothReset() async {
-    await Future.delayed(const Duration(milliseconds: 500));
-    o3dController.cameraOrbit(0, 95, 1); // Assuming radius is fixed at 1.0
-    o3dController.cameraTarget(0.20, 1.5, -11);
-    await Future.delayed(const Duration(milliseconds: 700));
-    _value = -11;
-    Timer.periodic(const Duration(milliseconds: 50), (timer) {
+    await Future.delayed(Duration(milliseconds: resetDelayMs));
+    _instantResetCameraPosition();
+    await Future.delayed(Duration(milliseconds: postResetDelayMs));
+
+    _value = resetValue;
+    _timer = Timer.periodic(Duration(milliseconds: timerDurationMs), (timer) {
       setState(() {
-        _value += 0.1; // Smaller increment for smoother transition
-        o3dController.cameraTarget(0.20, 1.5, _value);
+        _value += smoothIncrement;
+        _updateCameraTarget(_value);
         if (_value > 2) {
-          timer.cancel(); // Stop the reset timer
-          o3dController.cameraOrbit(
-              180, 95, 1); // Assuming radius is fixed at 1.0
-          o3dController.cameraTarget(0.20, 1.5, 2);
-          Future.delayed(
-              const Duration(milliseconds: 700), () => _isResetting = false);
+          timer.cancel();
+          _finalizeReset();
         }
       });
     });
   }
 
+  void _instantResetCameraPosition() {
+    o3dController.cameraOrbit(0, 95, 1);
+    _updateCameraTarget(resetValue);
+  }
+
+  void _finalizeReset() {
+    o3dController.cameraOrbit(180, 95, 1);
+    _updateCameraTarget(2);
+    Future.delayed(Duration(milliseconds: postResetDelayMs), () {
+      setState(() {
+        _isResetting = false;
+      });
+    });
+  }
+
+  void _updateCameraTarget(double value) {
+    o3dController.cameraTarget(0.20, 1.5, value);
+  }
+
   @override
   void dispose() {
-    _timer.cancel(); // Cancel the timer when the widget is disposed
+    _timer.cancel();
     super.dispose();
   }
 
@@ -74,84 +96,92 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
       body: SafeArea(
         child: Stack(
           children: [
-            O3D(
-              controller: o3dController,
-              src: AnimatedImages.mainCat,
-              alt: "A 3D model",
-              autoPlay: true,
-              autoRotate: false,
-              cameraControls: false,
-              animationName: animationName,
-              cameraOrbit: CameraOrbit(180, 95, 0.5),
-              cameraTarget: CameraTarget(0.20, 1.5, 2),
-              interactionPrompt: InteractionPrompt.none,
-              loading: Loading.eager,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Find Your Best',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        'Companion',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFEE9D2A),
-                        ),
-                      ),
-                      Text(
-                        'With CATS',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFEE9D2A),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      ),
-                      child: const Text(
-                        'Get Started',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            _build3DModel(),
+            _buildTextContent(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _build3DModel() {
+    return O3D(
+      controller: o3dController,
+      src: AnimatedImages.mainCat,
+      alt: "A 3D model",
+      autoPlay: true,
+      autoRotate: false,
+      cameraControls: false,
+      animationName: 'WalkClean',
+      cameraOrbit: CameraOrbit(180, 95, 0.5),
+      cameraTarget: CameraTarget(0.20, 1.5, 2),
+      interactionPrompt: InteractionPrompt.none,
+      loading: Loading.eager,
+    );
+  }
+
+  Widget _buildTextContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Find Your Best',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                'Companion',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFEE9D2A),
+                ),
+              ),
+              Text(
+                'With CATS',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFEE9D2A),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+              ),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
